103ef3939e789c148c0e1528d29014b6be35b0db
Kenny Kropp <kekropp>
2021-08-16 10:40:08 +0000

    Initial commit

---
+++ src/main/java/io/bitgrid/Application.java
@@ -0,0 +1,10 @@
+package io.bitgrid;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
